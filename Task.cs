84cb13787d1cdcd379559460e8158822c0d858f3
grzesiek <grzesiek@DESKTOP-VVMAVA4>
2016-07-21 23:33:22 +0200

    Class Task added

---
+++ Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskerClient
+{
+    class Task
+    {
+        int id { get; set; }
+        string name { get; set; }
+        string description { get; set; }
+        int priority { get; set; }
+        DateTime createDate { get; set; }
+        DateTime deadlineDate { get; set; }
+        bool isFinished = false;
+        public bool IsFinished
+        {
+            get { return isFinished; }
+            set { isFinished = value; }
+        }
+    }
+}
